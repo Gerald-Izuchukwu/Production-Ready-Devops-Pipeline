561d75943960a3f58adc54b6dc399df0ba6353c2
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2026-03-10 15:18:03 +0100

    First commit: Added Backend and Containerization

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>[
+    res.send('Hello Welcome to Production-Ready DevOps Pipeline!')
+])
+
+app.get('/health', (req, res)=>{
+    json_response = {
+        status: 'Healthy',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString()
+    }
+    console.log(json_response)
+    res.status(200).json(json_response)
+})
+
+app.get('/status', (req, res)=>{
+    json_response = {
+        status: 'Application is running smoothly!',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString()
+    }
+    console.log(json_response)
+    res.status(200).json(json_response)
+})
+
+app.post('/process', (req, res)=>{
+    const { data } = req.body;
+    if (!data) {
+        console.log(data)
+        return res.status(400).json({ error: 'Data is required' });
+    }
+    console.log(data)
+    res.status(200).json({ message: 'Data processed successfully',
+        receivedData: data
+    })
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+})
